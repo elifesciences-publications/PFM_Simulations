a26c079e6827629c1fcdd8f85cf7d1d85c1b536d
Sam Harrison <samuel.harrison@ndcn.ox.ac.uk>
2019-04-24 08:19:10 +0200

    Reduce PROFUMO spatial DoF correction
    Noise is almost independent over voxels, so don't need as strong a
    correction factor.

--- Overlap_functions/ICA_PROFUMO.sh
+++ Overlap_functions/ICA_PROFUMO.sh
@@ -11,16 +11,21 @@ homedir=$4
 
 # Create file list for melodic
 rm ${homedir}/Results/input_filelist.txt
-for filename in `ls ${homedir}/Results/Temp/*_${Option}.nii.gz` ; do 
-	echo $filename >> ${homedir}/Results/input_filelist_${Option}.txt
+for filename in `ls ${homedir}/Results/Temp/*_${Option}.nii.gz` ; do
+    echo $filename >> ${homedir}/Results/input_filelist_${Option}.txt
 done
 
 # run melodic
-fsl_sub -l logfiles -N M_${Option} melodic -i ${homedir}/Results/input_filelist_${Option}.txt  \
--o ${homedir}/Results/Melodic_${Option}.gica --tr=${tr} --nobet --nomask -a concat --disableMigp -d ${dim}
+fsl_sub -l logfiles -N M_${Option} \
+    melodic -i ${homedir}/Results/input_filelist_${Option}.txt  \
+    -o ${homedir}/Results/Melodic_${Option}.gica \
+    --tr=${tr} --nobet --nomask -a concat --disableMigp -d ${dim}
 
 # run PROFUMO (need to make sure we're on jalapeno18)
-nice -n 20 ~samh/bin/PROFUMO ${homedir}/Results/PFMsims_atlas_${Option}.json ${dim} \
-${homedir}/Results/PROFUMO_PFMsims_atlas_${Option} --nThreads 18 --useHRF ${tr} \
---hrfFile ~samh/PROFUMO/Scripts/DefaultHRF.phrf -d 0.05 > ${homedir}/Results/Output_PROFUMO_PFMsims_atlas_${Option}.txt
+nice -n 20 ~samh/bin/PROFUMO \
+    ${homedir}/Results/PFMsims_atlas_${Option}.json \
+    ${dim} \
+    ${homedir}/Results/PROFUMO_PFMsims_atlas_${Option} \
+    --useHRF ${tr} --hrfFile ~samh/PROFUMO/Scripts/DefaultHRF.phrf \
+    --nThreads 15 -d 0.5 > ${homedir}/Results/Output_PROFUMO_PFMsims_atlas_${Option}.txt
 
