1f7927fce7954d36cdafd2b08bd24586aca49d3c
Sam Harrison <samuel.harrison@ndcn.ox.ac.uk>
2019-04-29 09:27:14 +0100

    Run dual regression and save scores immediately
    Reintroduced into `RunTests.m`. Note `runDR.m` changed to accept group maps
    rather than subject time courses (was really triple-regression in past
    implementation).

--- Methods/runDR.m
+++ Methods/runDR.m
@@ -1,33 +1,34 @@
-function [ Ps, As ] = runDR( D, A, params )
-%Runs dual regression on the data set given a set of subject specific time
-%courses
+function [ P, A ] = runDR( D, Pg, params )
+% Runs dual regression on the data set to give a set of subject specific maps
+% and time courses
 
-nComps = size(A{1}{1}, 1);
+nComps = size(Pg, 2);
 
-%Regression to get subject maps
-Ps = cell(params.S,1);
+iPg = pinv(Pg - mean(Pg,1));
+
+% Regression for the time courses
+A = cell(params.S,1);
 for s = 1:params.S
-    %Extract data and time courses for this subject
+    A{s} = cell(params.R(s),1);
+    for r = 1:params.R(s)
+        % Extract time courses
+        Dsr = D{s}{r} - mean(D{s}{r},1);
+        A{s}{r} = iPg * Dsr;
+    end
+end
+
+% And for the subject maps
+P = cell(params.S,1);
+for s = 1:params.S
+    % Extract data and time courses for this subject
     As = zeros(nComps, params.T*params.R(s));
     Ds = zeros(params.V, params.T*params.R(s));
     for r = 1:params.R(s)
-        As(:, (r-1)*params.T+(1:params.T)) = A{s}{r};
-        Ds(:, (r-1)*params.T+(1:params.T)) = D{s}{r};
+        As(:, (r-1)*params.T+(1:params.T)) = A{s}{r} - mean(A{s}{r},2);
+        Ds(:, (r-1)*params.T+(1:params.T)) = D{s}{r} - mean(D{s}{r},2);
     end
-    %Pseudo-inverse to get Ps
-    Ps{s} = Ds * As' / (As * As');
-end
-
-%And repeat for the time courses
-As = cell(params.S,1);
-for s = 1:params.S
-    As{s} = cell(params.R(s),1);
-    %Pseudo-inverse
-    iPs = (Ps{s}' * Ps{s}) \ Ps{s}';
-    for r = 1:params.R(s)
-        %Extract time courses
-        As{s}{r} = iPs * D{s}{r};
-    end
+    % Pseudo-inverse to get P
+    P{s} = Ds * pinv(As);
 end
 
 end
