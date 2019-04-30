dc218a41245c510914b4520042b51c701bdd3243
Sam Harrison <samuel.harrison@ndcn.ox.ac.uk>
2019-04-30 12:40:26 +0100

    Return scores at the subject/run level
    I.e. don't prematurely average over these dimensions.

--- Scoring/calculateDecompositionAccuracy.m
+++ Scoring/calculateDecompositionAccuracy.m
@@ -6,9 +6,9 @@ function [ Pscore, Ascore, cPscore, cAscore ] ...
 %   courses, as well as the error between the spatial and temporal
 %   correlation matrices
 
-
+%%-----------------------------------------------------------------------------
 % First calculate the raw correlations of maps / time courses to ground
-% truth
+% truth, and use that to pair components
 
 %Calculate the map accuracies first
 %Take cosine similarity between true and inferred maps
@@ -33,21 +33,47 @@ cA = corrcov(cA);
 %Just the scores between the two
 cA = cA( 1:params.N, params.N+(1:params.iN) );
 
-
 %Match with the Hungarian algorithm and return scores sorted
 matching = Hungarian( -abs( cP + cA ) );
 
 %First find inds that match components
 [iGT, iInf] = ind2sub(size(matching), find(matching));
-signs = diag(diag(sign( cP(iGT,iInf) + cA(iGT,iInf) )));
+signs = diag(sign( cP(iGT,iInf) + cA(iGT,iInf) ));
 
-%Return scores in order of inferred components
-Pscore = NaN(params.iN, 1); Ascore = NaN(params.iN, 1);
-Pscore(iInf) = diag(cP(iGT, iInf)) .* diag(signs);
-Ascore(iInf) = diag(cA(iGT, iInf)) .* diag(signs);
-%Pscore(iInf) = cP(matching==1) .* sign(cP(matching==1) + cA(matching==1));
-%Ascore(iInf) = cA(matching==1) .* sign(cP(matching==1) + cA(matching==1));
+%%-----------------------------------------------------------------------------
 
+% Spatial accuracy - cosine similarity between subject maps
+Pscore = NaN(params.S, params.iN);
+for s = 1:params.S
+    cP = [P{s} infP{s}]' * [P{s} infP{s}];
+    cP = corrcov(cP);
+    % Just the scores between the two
+    cP = cP( 1:params.N, params.N+(1:params.iN) );
+    
+    % Return scores in order of inferred components
+    Pscore(s, iInf) = diag(cP(iGT, iInf)) .* signs;
+end
+
+%%-----------------------------------------------------------------------------
+
+% Repeat for the temporal accuracy
+% Take correlation between true and inferred time courses
+Ascore = NaN(sum(params.R), params.iN); sr = 1;
+for s = 1:params.S
+    for r = 1:params.R(s)
+        cA = cov([A{s}{r}; infA{s}{r}]');
+        cA = corrcov(cA);
+        % Just the scores between the two
+        cA = cA( 1:params.N, params.N+(1:params.iN) );
+        
+        % Return scores in order of inferred components
+        Ascore(sr, iInf) = diag(cA(iGT, iInf)) .* signs;
+        
+        sr = sr + 1;
+    end
+end
+
+%%-----------------------------------------------------------------------------
 
 %Now find how well the mode interactions have been calculated
 
@@ -61,7 +87,7 @@ for s = 1:params.S
     cP = P{s}(:,iGT)' * P{s}(:,iGT);
     cPz = r2z(corrcov(cP));
     
-    infcP = signs' * (infP{s}(:,iInf)' * infP{s}(:,iInf)) * signs;
+    infcP = (signs * signs') .* (infP{s}(:,iInf)' * infP{s}(:,iInf));
     infcPz = r2z(corrcov(infcP));
     
     %Now look at how similar the matrices are
@@ -73,34 +99,42 @@ for s = 1:params.S
     
 end
 
+%cPscore = sqrt( mean(cPscore)' );
+
+%%-----------------------------------------------------------------------------
+
 %Now repeat for the temporal correlations
 N = min(params.N, params.iN);
-cAscore = NaN(params.S, N*(N-1)/2);
+cAscore = NaN(sum(params.R), N*(N-1)/2); sr = 1;
 %Find the scores for each subjects corrcoef accuracy
 for s = 1:params.S
     
-    %Find z-scored partial correlations of real and observed timecourses
-    cA = 0; infcA = 0;
+    %Find z-scored correlations of real and observed timecourses
     for r = 1:params.R(s)
-        cA = cA + cov(A{s}{r}(iGT,:)');
-        infcA = infcA + signs * cov(infA{s}{r}(iInf,:)') * signs';
+        cA = cov(A{s}{r}(iGT,:)');
+        infcA = (signs * signs') .* cov(infA{s}{r}(iInf,:)');
+        
+        %cAz = r2z(corrcov(cA));
+        %infcAz = r2z(corrcov(infcA));
+        % Tikhonov regularised partials
+        cA = inv( corrcov(cA) + 0.01 * eye(N) );
+        cAz = r2z( -corrcov(cA) );
+        infcA = inv( corrcov(infcA) + 0.01 * eye(N) );
+        infcAz = r2z( -corrcov(infcA) );
+        
+        %Now look at how similar the matrices are
+        cAz = cAz(triu(ones(N),1)==1);
+        infcAz = infcAz(triu(ones(N),1)==1);
+        
+        %Save the RMS error between the correlation matrices
+        cAscore(sr,:) = (cAz - infcAz).^2;
+        
+        sr = sr + 1;
     end
-    % Tikhonov regularised partials
-    cA = inv( corrcov(cA) + 0.01 * eye(N) );
-    cAz = r2z(- corrcov(cA));
-    infcA = inv( corrcov(infcA) + 0.01 * eye(N) );
-    infcAz = r2z(- corrcov(infcA));
-    
-    %Now look at how similar the matrices are
-    cAz = cAz(triu(ones(N),1)==1);
-    infcAz = infcAz(triu(ones(N),1)==1);
-    
-    %Save the RMS error between the correlation matrices
-    cAscore(s,:) = (cAz - infcAz).^2;
-    
 end
 
-cPscore = sqrt( mean(cPscore)' );
-cAscore = sqrt( mean(cAscore)' );
+%cAscore = sqrt( mean(cAscore)' );
+
+%%-----------------------------------------------------------------------------
 
 end
